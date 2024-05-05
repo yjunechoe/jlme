1af46025c5e387cd3cc6772ec5cc464f0c6aa014
June Choe <yjchoe@sas.upenn.edu>
2024-05-04 22:28:03 -0400

    add test

--- R/aaa.R
+++ R/aaa.R
@@ -75,7 +75,7 @@ start_julia <- function(..., threads = NULL) {
     message(sprintf("Starting Julia with %i threads.", nthreads))
     Sys.setenv("JULIA_NUM_THREADS" = nthreads)
   }
-  JuliaConnectoR::startJuliaServer()
+  .jlme$port <- suppressMessages(JuliaConnectoR::startJuliaServer())
   Sys.setenv("JULIA_NUM_THREADS" = JULIA_NUM_THREADS %||% "")
   .jlme$is_setup <- TRUE
   invisible(TRUE)
---
+++ tests/testthat/test-model.R
@@ -0,0 +1,17 @@
+jlme_setup(restart = TRUE)
+
+test_that("model fitting works", {
+
+  jmod1 <- jlm(mpg ~ hp, mtcars)
+  expect_s3_class(jmod1, "jlme")
+  expect_s3_class(tidy(jmod1), "data.frame")
+  expect_s3_class(glance(jmod1), "data.frame")
+
+  jmod2 <- jlmer(Reaction ~ Days + (Days | Subject), lme4::sleepstudy)
+  expect_s3_class(jmod2, "jlme")
+  expect_s3_class(tidy(jmod2), "data.frame")
+  expect_s3_class(glance(jmod2), "data.frame")
+
+})
+
+stop_julia()
