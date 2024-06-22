97912c898718ad0d98553515517f382f3bb9d7e4
June Choe <yjchoe@sas.upenn.edu>
2024-06-22 21:18:55 +0900

    export jl_ helpers

--- R/jl_.R
+++ R/jl_.R
@@ -1,4 +1,39 @@
+#' Helpers for converting model specifications in R to Julia equivalents
+#' @name jl-helpers
+#' @keywords internal
+#' @examples
+#' jlme_setup(restart = TRUE)
+#'
+#' # (Setup) Model data in R
+#' x <- mtcars
+#' x$cyl_helm <- factor(x$cyl)
+#' contrasts(x$cyl_helm) <- contr.helmert(3)
+#' colnames(contrasts(x$cyl_helm)) <- c("4vs6", "4&6vs8")
+#'
+#' # Formula conversion with
+#' julia_formula <- jl_formula(mpg ~ am * cyl_helm)
+#' julia_formula
+#'
+#' # Data frame conversion
+#' julia_data <- jl_data(x)
+#' julia_data
+#'
+#' # Contrasts construction (`show_code = TRUE` pretty prints the Julia code)
+#' julia_contrasts <- jl_contrasts(x, show_code = TRUE)
+#' julia_contrasts
+#'
+#' # Family conversion
+#' julia_family <- jl_family("binomial")
+#' julia_family
+#'
+#' stop_julia()
+NULL
+
+#' @rdname jl-helpers
+#' @param x A string or formula object
+#' @export
 jl_formula <- function(x) {
+  if (is_jl(x)) return(x)
   stopifnot(is.character(x) || inherits(x, "formula"))
   if (requireNamespace("JuliaFormulae", quietly = TRUE)) {
     x <- JuliaFormulae::julia_formula(x)
@@ -16,22 +51,36 @@ jl_formula <- function(x) {
   }
 }
 
-jl_contrasts <- function(df, cols = NULL, ..., show_code = FALSE) {
+#' @rdname jl-helpers
+#' @param df A data frame
+#' @param cols A subset of columns to make contrast specifiations for
+#' @param show_code Whether to print corresponding Julia code as a side-effect
+#' @export
+jl_contrasts <- function(df, cols = NULL, show_code = FALSE) {
   dict <- construct_contrasts(df, cols = cols)
   if (show_code) {
     cat(dict)
-    return(invisible(NULL))
   }
   jl_evalf(dict)
 }
 
+#' @rdname jl-helpers
+#' @export
 jl_data <- function(df) {
+  if (is_jl(df)) return(df)
   fct_cols <- Filter(is.factor, df)
   df[, colnames(fct_cols)] <- lapply(fct_cols, as.character)
-  df
+  JuliaConnectoR::juliaPut(df)
 }
 
+#' @rdname jl-helpers
+#' @param family The distributional family as string or `<family>` object
+#' @export
 jl_family <- function(family = c("gaussian", "binomial", "poisson")) {
+  if (is_jl(family)) return(family)
+  if (inherits(family, "family")) {
+    family <- family$family
+  }
   if (is.character(family)) {
     family <- match.arg(family)
     family <- switch(
@@ -41,8 +90,6 @@ jl_family <- function(family = c("gaussian", "binomial", "poisson")) {
       "poisson"  = "Poisson"
     )
     family <- jl_evalf("GLM.%s()", family)
-    family
-  } else if (is_jl(family)) {
     family
   } else {
     stop("Invalid input to the `family` argument.")
